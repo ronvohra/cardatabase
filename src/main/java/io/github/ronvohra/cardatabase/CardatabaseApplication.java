9295e37b6fe642931d1cdfe7a5898db43aef2518
Raunaq Vohra <ronvohra@gmail.com>
2024-07-27 19:20:10 +0100

    Initial commit

---
+++ src/main/java/io/github/ronvohra/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,13 @@
+package io.github.ronvohra.cardatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ronvohra/cardatabase/CardatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ronvohra.cardatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
